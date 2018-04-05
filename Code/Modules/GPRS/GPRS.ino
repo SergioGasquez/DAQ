99311969c9c62fe75532c4e7b2aa8cc83788f732
Sergio Gasquez Arcos <sergiogasquez@correo.ugr.es>
2018-04-05 15:50:02 +0200

    Changes on GPRS module

--- Code/Modules/GPRS/GPRS.ino
+++ Code/Modules/GPRS/GPRS.ino
@@ -1,55 +1,49 @@
 /*
-Sketch: GPRS Connection Test
-
-by R. Pelayo
-Copyright (c) January 2018 Teach Me Micro
-
+Sketch: GPRS HTTP Test
 */
-#include <gprs800.h>
+#include <gprs.h>
 #include <SoftwareSerial.h>
 
-char http_cmd[] = "GET / HTTP/1.0\r\n\r\n";       //server request
-char website[] = "www.teachmemicro.com";          //website domain name or ip address
-char apn[] = "gprs.pepephone.com";             //APN of your cellular network provider
-char uname[] = "Pepephone";                  //APN username
-char pword[] = "";                                //APN password
-int port = 80;                                    //connection port, usually 80 for HTTP
-
+char http_cmd[] = "GET exploreembedded.com/wiki/images/1/15/Hello.txt HTTP/1.0\r\n\r\n";
 char buffer[512];
 
-GPRS800 gprs;
+GPRS gprs;
 
 void setup() {
   Serial.begin(9600);
   while(!Serial);
-  Serial.println("GPRS HTTP Connection Example");  
-  Serial.print("Connecting via ");
-  Serial.println(apn);
-  
-  while(!gprs.init(apn, uname, pword)) { 
+  Serial.println("GPRS - HTTP Connection Test...");  
+  gprs.preInit();
+  while(0 != gprs.init()) {
+     delay(1000);
+     Serial.println("init error");
+  }  
+  while(!gprs.join("airtelgprs.com")) {  //change "cmnet" to your own APN
+      Serial.println("Error joining GPRS network");
       delay(2000);
   }
+  // successful DHCP
   Serial.print("IP Address is ");
   Serial.println(gprs.getIPAddress());
   
-  Serial.print("Initialization successful. Connecting to ");
-  Serial.println(website);
+  Serial.println("Init success, connecting to exploreembedded.com ...");
   
-  if(gprs.connectTCP(website, port)) {
-      Serial.print("Connected to ");
-      Serial.println(website);
+  if(0 == gprs.connectTCP("exploreembedded.com", 80)) {
+      Serial.println("Successfully connected to exploreembedded.com!");
   }else{
-      Serial.println("Connection error");
+      Serial.println("connect error");
       while(1);
   }
 
-  Serial.println("Fetching server response");
-  if(gprs.sendTCP(http_cmd))
+  Serial.println("waiting to fetch...");
+  if(0 == gprs.sendTCPData(http_cmd))
   {      
-    gprs.showResponse();
+    gprs.serialDebug();
   }
-  gprs.closeTCP();
-
+  
+//  gprs.closeTCP();
+//  gprs.shutTCP();
+//  Serial.println("close");  
 }
 
 void loop() {    
